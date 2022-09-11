7162028693415ca0a06b914d933df54df4dc45eb
Piotr Dyrda <piotr.dyrda88@gmail.com>
2022-09-11 22:04:05 +0200

    db connection, nest packages update, swagger integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { TaskModule } from './task/task.module';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TaskModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i7ia4sr.mongodb.net/?retryWrites=true&w=majority`,
+      { dbName: 'task-menager' },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
